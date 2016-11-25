d00685e2f246737edfea8789293c326d1d642da5
Michael Adam <madamt0001@gmail.com>
2016-11-25 23:07:23 +0800

    Update

--- resources/views/admin/co-admin-assign-block-view.blade.php
+++ resources/views/admin/co-admin-assign-block-view.blade.php
@@ -10,7 +10,7 @@
     <div id="page-wrapper">
         <div class="row">
             <div class="col-lg-12">
-                <h3 class="page-header">View Block Assignemnt to Adviser</h3>
+                <h3 class="page-header">View Block Assignment to Adviser</h3>
             </div>
             
         </div>
