488375120eab5245f5447cd7c3ef9a771faeb7bd
Michael Adam <madamt0001@gmail.com>
2016-12-12 13:55:52 +0800

    Fixed Mobile Number Field

--- resources/views/admin/admin-profile-update.blade.php
+++ resources/views/admin/admin-profile-update.blade.php
@@ -39,7 +39,7 @@
                                 <input type="email" name="email" class="form-control" value="{{ $admin->email }}" placeholder="Email Address" />
                             </div>
                             <div class="form-group">
-                                <input type="text" name="mobile" class="form-control" value="{{ $admin->mobile }}" placeholder="11 Digit Mobile Number" />
+                                <input type="number" name="mobile" class="form-control" value="{{ $admin->mobile }}" placeholder="11 Digit Mobile Number" />
                             </div>
                             <div class="form-group">
                                 <input type="text" name="birthday" class="form-control" value="{{ date('m/d/Y', strtotime($admin->birthday)) }}" placeholder="MM/DD/YYY" autofocus="" />
